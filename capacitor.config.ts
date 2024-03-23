8fb4611e7e109254d4e0300cf4c9bc4c91973a8a
RobDav22 <robertodavila735@gmail.com>
2024-03-23 12:33:18 -0600

    Proyecto ionic

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'miAppMovil-2063584',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
